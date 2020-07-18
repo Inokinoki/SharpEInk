144ec8941eafd87aea78a3f6fe1e863593a5d365
Inoki <veyx.shaw@gmail.com>
2020-07-18 20:46:16 +0200

    Add a workable preview for fake epd

---
+++ WinIoTEPDInk/FakeEpd.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace WinIoTEPDInk
+{
+    class FakeEpd : IEpd
+    {
+        private Image _image = null;
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public bool Sleeping { get; }
+
+        private byte[] buffer;
+
+        public FakeEpd(EpdModel model, Image image)
+        {
+            Width   = EpdResolution.WIDTH[(int)model];
+            Height  = EpdResolution.HEIGHT[(int)model];
+
+            buffer = new byte[Width / 8 * Height];  // Create a buffer for image
+
+            _image = image;     // EPD preview will be updated to this control
+        }
+
+        public void ClearFrameMemory(byte data)
+        {
+            for (int i = 0; i < Width / 8 * Height; i++)
+            {
+                buffer[i] = data;
+            }
+        }
+
+        public async void DisplayFrameAsync()
+        {
+            if (Sleeping)
+            {
+                Debug.WriteLine(@"Fake Module sleeping, I'll do nothing");
+                return;
+            }
+
+            if (_image != null)
+            {
+                /* Generate gray image */
+                byte[] generatedImage = new byte[4 * Width * Height];
+                for (int i = 0; i < Width * Height; ++i)
+                {
+                    generatedImage[4 * i] = 255;
+                    generatedImage[4 * i + 1] = 255;
+                    generatedImage[4 * i + 2] = 255;
+                    generatedImage[4 * i + 3] = 255;
+                    if ((buffer[i / 8] & (0x80 >> (i % 8))) == 1)
+                    {
+                        generatedImage[4 * i] = 0;
+                        generatedImage[4 * i + 1] = 0;
+                        generatedImage[4 * i + 2] = 0;
+                        generatedImage[4 * i + 3] = 0;
+                    }
+                    else
+                    {
+                        generatedImage[4 * i] = 255;
+                    }
+                }
+
+                IStorageFolder applicationfolder = ApplicationData.Current.LocalFolder;
+                Debug.WriteLine(applicationfolder.Path);
+                IStorageFile saveFile = await applicationfolder.CreateFileAsync("test.png", CreationCollisionOption.ReplaceExisting);
+                using (var fileStream = await saveFile.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
+                    encoder.SetPixelData(BitmapPixelFormat.Rgba8, BitmapAlphaMode.Ignore,
+                       (uint)Width, (uint)Height, 75, 75, generatedImage.ToArray());
+                    await encoder.FlushAsync();
+                }
+
+                /* Create a stream to store encoded bitmap data */
+                //IRandomAccessStream stream = new InMemoryRandomAccessStream();
+                //stream.Size = (ulong)(4 * Width * Height);
+
+                // Encode image
+                //var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, stream);
+                //encoder.SetPixelData(BitmapPixelFormat.Rgba8, BitmapAlphaMode.Ignore,
+                //    (uint)Width, (uint)Height, 75, 75, generatedImage.ToArray());
+                //await stream.FlushAsync();
+                //stream.Seek(0);
+
+                //await stream.WriteAsync(generatedImage.AsBuffer());
+                //stream.Seek(0);
+
+                //Debug.WriteLine(stream.Size);
+                Stream s = await applicationfolder.OpenStreamForReadAsync("test.png");
+
+                /* Create a bitmap source */
+                BitmapImage imageSource = new BitmapImage();
+                await imageSource.SetSourceAsync(s.AsRandomAccessStream());
+                //imageSource.SetSource(stream);
+                // await imageSource.SetSourceAsync(stream);
+
+                BitmapImage One = new BitmapImage(new Uri("ms-appx:///Assets/StoreLogo.png"));
+
+                /* Update image to the control */
+                _image.Source = imageSource;
+                _image.Width = Width;
+                _image.Height = Height;
+                Debug.WriteLine("Loaded to image OK");
+                Debug.WriteLine(_image.Name);
+                Debug.WriteLine(Width);
+                Debug.WriteLine(Height);
+            }
+        }
+
+        public void Reset()
+        {
+            if (Sleeping)
+            {
+                Debug.WriteLine(@"Fake Module sleeping, I'll do nothing");
+                return;
+            }
+
+            Debug.WriteLine(@"Fake Module reset");
+
+            // FIXME: what should happen when reset
+        }
+
+        public void SetFrameMemory(byte[] image_buffer, int startX, int startY, int image_width, int image_height)
+        {
+            if (Sleeping)
+            {
+                Debug.WriteLine(@"Fake Module sleeping, I'll do nothing");
+                return;
+            }
+
+        }
+
+        public void SetFrameMemory(byte[] image_buffer, int image_width, int image_height)
+        {
+            if (Sleeping)
+            {
+                Debug.WriteLine(@"Fake Module sleeping, I'll do nothing");
+                return;
+            }
+
+            SetFrameMemory(image_buffer, 0, 0, image_width, image_height);
+        }
+
+        public void SetFrameMemory(byte[] image_buffer)
+        {
+            if (Sleeping)
+            {
+                Debug.WriteLine(@"Fake Module sleeping, I'll do nothing");
+                return;
+            }
+
+            for (int i = 0; i < Width / 8 * Height && i < image_buffer.Length; i++)
+            {
+                // Fill the buffer
+                buffer[i] = image_buffer[i];
+            }
+        }
+
+        public void Sleep()
+        {
+            Debug.WriteLine(@"Fake Module sleeping...");
+        }
+    }
+}
---
+++ WinIoTEPDInk/IEpd.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace WinIoTEPDInk
+{
+    public enum EpdModel
+    {
+        EPD1IN54 = 0,
+        EPD1IN54B,
+        EPD1IN54C,
+        EPD2IN13,
+        EPD2IN13B,
+        EPD2IN7,
+        EPD2IN7B,
+        EPD2IN9,
+        EPD2IN9B,
+        EPD4IN2,
+        EPD4IN2B,
+        EPD7IN5,
+        EPD7IN5B
+    }
+
+    internal sealed class EpdResolution
+    {
+        public static int[] WIDTH = new int[]
+        {
+            200,    // EPD1IN54
+            200,    // EPD1IN54B
+            200,    // EPD1IN54C
+            128,    // EPD2IN13
+            128,    // EPD2IN13B
+            176,    // EPD2IN7
+            176,    // EPD2IN7B
+            128,    // EPD2IN9
+            128,    // EPD2IN9B
+            400,    // EPD4IN2
+            400,    // EPD4IN2B
+            640,    // EPD7IN5
+            640,    // EPD7IN5B
+        };
+
+        public static int[] HEIGHT = new int[]
+        {
+            200,    // EPD1IN54
+            200,    // EPD1IN54B
+            200,    // EPD1IN54C
+            250,    // EPD2IN13
+            250,    // EPD2IN13B
+            264,    // EPD2IN7
+            264,    // EPD2IN7B
+            296,    // EPD2IN9
+            296,    // EPD2IN9B
+            300,    // EPD4IN2
+            300,    // EPD4IN2B
+            384,    // EPD7IN5
+            384,    // EPD7IN5B
+        };
+    }
+
+    interface IEpd
+    {
+        /**
+         * Functional
+         */
+        void Reset();
+        void DisplayFrameAsync();
+        void Sleep();
+
+        /**
+         * Set image
+         */
+        void SetFrameMemory(byte[] image_buffer, int startX, int startY, int image_width, int image_height);
+        void SetFrameMemory(byte[] image_buffer);
+
+        /**
+         * Clear image
+         */
+        void ClearFrameMemory(byte color);
+    }
+}
