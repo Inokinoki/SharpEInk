807730e404ea64d965bcca512887b38cfda831e3
Inoki <veyx.shaw@gmail.com>
2020-07-19 20:34:58 +0200

    Add control UI to XAML (and its control code)

--- WinIoTEPDInk/MainPage.xaml.cs
+++ WinIoTEPDInk/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -26,11 +27,16 @@ namespace WinIoTEPDInk
         {
             this.InitializeComponent();
 
-            FakeEpd epd = new FakeEpd(EpdModel.EPD2IN9, display);
+            epd = new FakeEpd(EpdModel.EPD2IN9, display);
             
-            epd.SetFrameMemory(TestData.WAVESHARE_SAMPLE, 128, 200);
-            epd.DisplayFrameAsync();
+            epd.SetFrameMemory(TestData.RANDOM_SAMPLE, 128, 296, 0, 0);
+            epd.DisplayFrame();
+
+            realEpd = new SPIEpd(EpdModel.EPD2IN9, reset_pin: 17, cs_pin: 8, dc_pin: 27, busy_pin: 22);
         }
+
+        private SPIEpd realEpd;
+        private FakeEpd epd;
 
         private double originWidth = -1;
         private double originHeight = -1;
@@ -58,6 +64,36 @@ namespace WinIoTEPDInk
         private void SizeResetButton_Click(object sender, RoutedEventArgs e)
         {
             sizeSlider.Value = 100;
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            realEpd.connectSPIAsync();
+        }
+
+        private void SyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            Sync();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+            Reset();
+        }
+
+        private async Task Sync()
+        {
+            await realEpd.SetFrameMemoryAsync(TestData.RANDOM_SAMPLE);
+            await realEpd.DisplayFrameAsync();
+        }
+
+        private async Task Reset()
+        {
+            await realEpd.ClearFrameMemoryAsync(0xAA);
+            await realEpd.DisplayFrameAsync();
+            await realEpd.ClearFrameMemoryAsync(0xFF);
+            await realEpd.DisplayFrameAsync();
         }
     }
 }
